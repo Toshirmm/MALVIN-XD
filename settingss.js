6036622aeba53ce9277a46f47d6e5fb9ce3edeb5
Toshirmm <144406472+Toshirmm@users.noreply.github.com>
2025-09-05 20:51:41 -0400

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "malvin~wqNRTIzB#eeOdDRJEDS1mJL2mBBmts8Mxrcz5z2MLZlAvEcnd87M",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: "malvin~9q1FjSpT#gOHVHLl0r-Q-aO7rgym7mvHIaW5_tJ16ONmkW5Vt1_g",  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
   PREFIX: "\" // prefix (e.g., ., /, !, *)
 };
